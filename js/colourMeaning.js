1c3b2e67f196b45dae229ae47a9d7ea878d52e92
Carolyn McNeillie <hello@carolynmcneillie.com>
2018-02-08 15:29:11 -0500

    some styling, some colour definitions added

--- js/colourMeaning.js
+++ js/colourMeaning.js
@@ -1,11 +1,15 @@
 const colours = {}
 
 colours.colourMeaning = { //holds values for fortune at end
-    red: `<p>Your aura is red.</p>`,
-    orange: `<p>Your aura is orange</p>`,
-    green: `<p>Your aura is green</p>`,
-    blue: `<p>Your aura is blue</p>`,
-    turquoise: `<p>Your aura is turquoise</p>`,
-    purple: `<p>Your aura is purple</p>`,
-    white: `<p>Your aura is white</p>`,
+    red: `<p class="profile">People with red auras cut a winding and far-ranging path though life, gathering stories like souveniers along the way. You are innately curious, but you also have a tendancy to lose interest quickly. Some red auras leave a trail of broken hearts and unkept promises in their wake. (Not to mention a metaphoroical closet filled with unfinished projects.)</p><p class="profile">When you put your mind to it, you can move mountains.</p>`,
+    orange: `<p class="profile">People with orange auras are the beating heart of any gathering. You are gragarious, generous, good-hearted, and kind. You love to be around people, whether you are the center of attention or just another face in the crowd. And people love to be around you too, because you put them at ease.</p><p class="profile">People with orange auras live successful and happy lives.</p>`,
+    yellow: `<p class="profile">People with yellow auras are independant, logical, and whip-smart. You read people quickly and with stunning accuracy. You don't suffer fools, but you are the first to recognize brilliance in others. Your sharp insight can give you a tendancy to be critical of yourself, but you are as brilliant as they come, and deep down you know that too. You are an unorthodox and unconventional thinker.</p><p class="profile">Many yellow auras are delightfully eccentric. </p>`,
+    green: `<p class="profile">Your aura is green</p>`,
+    blue: `<p class="profile">Your aura is blue</p>`,
+    turquoise: `<p class="profile">Your aura is turquoise</p>`,
+    purple: `<p class="profile">Your aura is purple</p>`,
+    white: `<p class="profile">Your aura is white</p>`,
 }
+
+//RED
+//The only thing more firey than your temper is your passion. You are brave and adventurious. It's difficult for you to share the spotlight, and you prefer to be in a position of authority.
