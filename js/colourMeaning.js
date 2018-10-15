a5880d25a06effc2c86430ded3215779afff067d
Benjamin Soyka <37779854+bsoyka@users.noreply.github.com>
2018-10-14 19:38:25 -0700

    Added missing semicolons

--- js/colourMeaning.js
+++ js/colourMeaning.js
@@ -1,4 +1,4 @@
-const colours = {}
+const colours = {};
 
 colours.colourMeaning = { //holds values for fortune at end
     red: `<p>People with red auras cut a winding and far-ranging path though life, gathering stories like souvenirs along the way. You are innately curious, but have a tendency to lose interest quickly. Some red auras leave a trail of broken hearts and unkept promises in their wake. But when you put your mind to it, you can move mountains.</p>`,
@@ -9,4 +9,4 @@ colours.colourMeaning = { //holds values for fortune at end
     turquoise: `<p>People with turquoise auras are healers. You are sensitive, compassionate, and an excellent listener. You are steady and unflappable. You see the potential in those around you, and you thrive on lending your courage and wisdom in support of your loved ones.</p>`,
     purple: `<p>People with purple auras are philosophical, inquiring and intuitive. You have bottomless curiosity and love to learn. You keep a small and tight group of friends, but your warmth and enthusiasm for sharing your interests are widely admired. Animals love you.</p>`,
     white: `<p>People with white auras are exceptionally gifted. Your life lesson is to discover how to use your gifts wisely. You are sensitive, and able to use your spiritual intuition in practical ways. You are versatile and adaptable, and naturally reflect your surroundings and the people around you. Success comes easily to you.</p>`,
-}
+};
